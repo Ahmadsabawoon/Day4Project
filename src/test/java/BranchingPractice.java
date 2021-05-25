bd9e39bdda892d77894cff775751d74bba7a6d87
Ahmad Seyar Sabawoon <seyarkhan143@gmail.com>
2021-05-25 16:23:46 -0700

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -3,5 +3,10 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        // create new project
+        // enable version control
+        // added .getingnore with .idea folder and .iml file
+        // made initial commit by keeping track of all files
+        // shared project on github
     }
 }
