e9a5393d9eceb2c2d2abaa64d55547d81bfe1119
Ahmad Seyar Sabawoon <seyarkhan143@gmail.com>
2021-05-25 18:18:52 -0700

    Tc 001 has been completed

--- src/test/java/TestCase001.java
+++ src/test/java/TestCase001.java
@@ -2,4 +2,7 @@ public class TestCase001 {
 
     //  work1
     // work 2
+    // finished this test case ready to push to remote
+    // ready to open pull request(permission to merger)
+    // discusses and eventually merge the code
 }
